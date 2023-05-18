334ffb3bf521038a987ed2dc0466aa18740273fd
MEO MEO <handestd@gmail.com>
2023-05-18 14:50:38 +0700

    complete software

--- Mac Address Tool/Form1.Designer.cs
+++ Mac Address Tool/Form1.Designer.cs
@@ -30,6 +30,13 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
@@ -41,13 +48,6 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -85,6 +85,79 @@
             this.tabPage1.Text = "Generator";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(239, 120);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(145, 20);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(236, 97);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 16);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Amount";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Uppercase",
+            "Lowercase"});
+            this.comboBox2.Location = new System.Drawing.Point(22, 119);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(145, 21);
+            this.comboBox2.TabIndex = 10;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(19, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Case:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "00-10-FA-63-38-4A",
+            "00.10.FA.63.38.4A",
+            "00:10:FA:63:38:4A",
+            "0010FA63384A"});
+            this.comboBox1.Location = new System.Drawing.Point(239, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(145, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(236, 19);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(133, 16);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "MAC Address format:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(154, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Maximum 6 character (A-F,0-9).";
+            // 
             // label4
             // 
             this.label4.AutoSize = true;
@@ -120,6 +193,7 @@
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(145, 20);
             this.textBox4.TabIndex = 1;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // textBox3
             // 
@@ -129,6 +203,7 @@
             this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.textBox3.Size = new System.Drawing.Size(221, 395);
             this.textBox3.TabIndex = 0;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // tabPage2
             // 
@@ -190,76 +265,6 @@
             this.label1.Size = new System.Drawing.Size(19, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "[0]";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(19, 68);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(154, 13);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Maximum 6 character (A-F,0-9).";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(236, 19);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(187, 16);
-            this.label7.TabIndex = 7;
-            this.label7.Text = "MAC Address prefix: (optional)";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "00-10-FA-63-38-4A",
-            "00.10.FA.63.38.4A",
-            "00:10:FA:63:38:4A",
-            "0010FA63384A"});
-            this.comboBox1.Location = new System.Drawing.Point(239, 41);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(145, 21);
-            this.comboBox1.TabIndex = 8;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Items.AddRange(new object[] {
-            "Uppercase",
-            "Lowercase"});
-            this.comboBox2.Location = new System.Drawing.Point(22, 119);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(145, 21);
-            this.comboBox2.TabIndex = 10;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(19, 97);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(42, 16);
-            this.label6.TabIndex = 9;
-            this.label6.Text = "Case:";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(236, 97);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(52, 16);
-            this.label8.TabIndex = 11;
-            this.label8.Text = "Amount";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(239, 120);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(145, 20);
-            this.textBox5.TabIndex = 13;
             // 
             // Form1
             // 
--- Mac Address Tool/Form1.cs
+++ Mac Address Tool/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net.Mail;
 using System.Net.NetworkInformation;
@@ -14,10 +15,83 @@ namespace Mac_Address_Tool
 {
     public partial class Form1 : Form
     {
+
+        string format = "";
+        int amount = 1;
+        string mycase = "up";
+        string prefix = "";
         public Form1()
         {
             InitializeComponent();
+            if (File.Exists("amount.txt"))
+            {
+                try
+                {
+                    amount = int.Parse(File.ReadAllText("amount.txt").Trim());
+                    textBox5.Text = amount.ToString();
+                }
+                catch { }
+            }
+            else
+            {
+                textBox5.Text = "1";
+            }
+            if (File.Exists("case.txt"))
+            {
+                string value = File.ReadAllText("case.txt");
+                if (value == "Uppercase")
+                {
+                    mycase = "up";
+                    comboBox2.SelectedIndex = 0;
+                }
+                else if (value == "Lowercase")
+                {
+                    mycase = "lower";
+                    comboBox2.SelectedIndex = 1;
+                }
+            }
+            else
+            {
+                comboBox2.SelectedIndex = 0;
+            }
+            if (File.Exists("prefix"))
+            {
+                prefix = File.ReadAllText("prefix.txt");
+                textBox4.Text = prefix;
+            }
+            if (File.Exists("format.txt"))
+            {
+                string value = File.ReadAllText("format.txt");
+                if (value.Contains("."))
+                {
+                    format = ".".ToString();
+                    comboBox1.SelectedIndex = 1;
+                }
+                else if (value.Contains(":"))
+                {
+                    format = ":".ToString();
+                    comboBox1.SelectedIndex = 2;
+                }
+                else if (value.Contains("-"))
+                {
+                    format = "-".ToString();
+                    comboBox1.SelectedIndex = 0;
+                }
+                else
+                {
+                    format = "";
+                    comboBox1.SelectedIndex = 3;
+                }
+            }
+            else
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+
         }
+
+     
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -35,15 +109,13 @@ namespace Mac_Address_Tool
                     }
                     catch 
                     {
-                        MessageBox.Show($"{item} is not valid format");
+                      
                     }
 
                     if (MACAddress != null) 
                     {
-                        var vendorInfo = setting.addressMatcher.FindInfo(MACAddress);
-                      
-                        textBox2.AppendText(item + " " + $"\t{vendorInfo}" + Environment.NewLine);
-                        
+                        var vendorInfo = setting.addressMatcher.FindInfo(MACAddress);                      
+                        textBox2.AppendText(item + " " + $"\t{vendorInfo}" + Environment.NewLine);                       
                     }
 
 
@@ -53,34 +125,57 @@ namespace Mac_Address_Tool
         }
         private static readonly Random Random = new Random();
 
-    
 
-        public static string GetRandomMac()
+        private static string MACToString(byte[] Bytes, string format)
         {
-            string[] macBytes = new[]
-            {
-            Random.Next(1, 256).ToString("X"),
-            Random.Next(1, 256).ToString("X"),
-            Random.Next(1, 256).ToString("X"),
-            Random.Next(1, 256).ToString("X"),
-            Random.Next(1, 256).ToString("X"),
-            Random.Next(1, 256).ToString("X")
-        };
 
-            return string.Join("-", macBytes);
+            return BitConverter.ToString(Bytes).Replace("-", format).ToUpper();
         }
+        public static string CreateMAC(string format)
+        {
+            Random RM = new Random();
+
+            byte[] Bytes = new byte[6];
+            RM.NextBytes(Bytes);
+
+            Bytes[0] = (byte)(Bytes[0] | 0x02);
+            Bytes[0] = (byte)(Bytes[0] & 0xfe);
+
+            return MACToString(Bytes, format);
+        }
+
+       
+        
         private void button2_Click(object sender, EventArgs e)
         {
-            //MessageBox.Show(GetRandomMac());
-       
-            MessageBox.Show(GetSignatureRandomMac("-", "B2"));
+            //MessageBox.Show(GetRandomMac());     
+            for (int i = 0; i < amount; i++)
+            {
+                string item = GetSignatureRandomMac(format, prefix);
 
+                if (mycase == "up")
+                {
+                    item = item.ToUpper();
+                }
+                else
+                {
+                    item = item.ToLower();
+                }
+
+                textBox3.AppendText(item+Environment.NewLine);
+            }       
         }
 
 
         public static string GetSignatureRandomMac(string separator, string generic = "AA")
         {
             string[] macBytes = null;
+
+            if (generic.Length == 0)
+            {
+                return CreateMAC(separator);
+            }
+
             if (generic.Length == 1)
             {
                 macBytes = new[]
@@ -155,6 +250,61 @@ namespace Mac_Address_Tool
             }
 
             return string.Join(separator, macBytes);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            File.WriteAllText("format.txt", comboBox1.SelectedItem.ToString());
+
+            switch (comboBox1.SelectedIndex) 
+            {
+                case 0:
+                    format = "-".ToString(); break;                
+                case 1:
+                    format = ".".ToString(); break;
+                case 2: 
+                    format = ":".ToString(); break;
+                case 3:
+                    format = "".ToString(); break;
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            File.WriteAllText("amount.txt", textBox5.Text);
+            try
+            {
+                amount = int.Parse(textBox5.Text);
+            }
+            catch { }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            File.WriteAllText("case.txt", comboBox2.SelectedItem.ToString());
+
+            if (comboBox2.SelectedIndex == 0)
+            {
+                mycase = "up";
+            }
+            else
+            {
+                mycase = "lower";
+            }
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            File.WriteAllText("prefix.txt", textBox4.Text);
+            prefix = textBox4.Text.ToUpper();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            label4.Text = "[" + textBox3.Lines.Length + "]"; 
+                
         }
     }
 }
